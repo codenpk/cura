aef6213f3337535b3d54d2714aa89acee69ade2b
ct5845 (home) <ct5845@gmail.com>
2016-03-26 17:04:51 +0000

    Added simple Jasmine test, check Travis CI integration

---
+++ source/server/bootstrap.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('Server Creation', () => {
+    it('works', () => {
+        expect(true).toBe(true);
+    });
+
+    it('fails', () => {
+        expect(true).toBe(false);
+    });
+});
