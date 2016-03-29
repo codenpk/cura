da5bcb653a3cd054f1cd12dba3ca366a9dc8f801
Chris Turner <ct5845@gmail.com>
2016-03-29 10:40:31 +0100

    Change config workings. Trying to ignore .spec files for code climate.

--- source/server/config/config.js
+++ source/server/config/config.js
@@ -1,7 +1,25 @@
-let config = require('./dev.config');
+let config = {
+    session: process.env.SESSIONTOKEN || 'sessionToken',
+    activation: process.env.ACTIVATIONTOKEN || 'activationToken',
+    server: {
+        port: process.env.SERVERPORT || 3000,
+        uri: process.env.SERVERURI || 'http://localhost:3000'
+    },
+    mongo: {
+        uri: process.env.MONGOURI || 'mongodb://localhost/cura',
+        options : {
+            db : {
+                safe: true
+            }
+        }
+    }
+};
 
 if (process.env.NODE_ENV === 'test') {
-    config = require('./test.config');
+    config.server.port = process.env.SERVERPORT || 3001;
+    config.server.uri = process.env.SERVERURI || 'http://localhost:3001';
+
+    config.mongo.uri = process.env.MONGOURI || 'mongodb://localhost/cura-test'
 }
 
-export default config.default;
+export default config;
